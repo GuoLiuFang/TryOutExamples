b928a7588115f27083ac48a918b94fae735b3c6e
LiuFangGuo <guoliufang@tigerjoys.com>
2016-06-29 16:29:19 +0800

    验证生波的修补数据

--- src/main/java/com/funcoming/mapper/SedAwkMapper.java
+++ src/main/java/com/funcoming/mapper/SedAwkMapper.java
@@ -6,17 +6,26 @@ import org.apache.hadoop.io.Text;
 import org.apache.hadoop.mapreduce.Mapper;
 
 import java.io.IOException;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 /**
  * Created by LiuFangGuo on 6/28/16.
  */
 public class SedAwkMapper extends Mapper<LongWritable, Text, Text, NullWritable> {
+    private final String patternString = "2015-0[1-5]";
+    private final Pattern patterner = Pattern.compile(this.patternString);
     private Text line = new Text();
 
     @Override
     protected void map(LongWritable key, Text value, Context context) throws IOException, InterruptedException {
-        String missingFields = context.getConfiguration().get("missingFields");
-        this.line.set(value.toString() + missingFields);
-        context.write(this.line, NullWritable.get());
+        String[] split = value.toString().split("[|]");
+//        String[] split = value.toString().split("\\|");
+        Matcher matcher = this.patterner.matcher(split[split.length - 1]);
+        if (matcher.find()) {
+            String missingFields = context.getConfiguration().get("missingFields");
+            this.line.set(value.toString() + missingFields);
+            context.write(this.line, NullWritable.get());
+        }
     }
 }
