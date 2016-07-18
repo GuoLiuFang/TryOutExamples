770f72b9563b4beabdab80da85eb63f7858e103e
LiuFangGuo <guoliufang@tigerjoys.com>
2016-07-18 18:40:58 +0800

    test DistributedCacheFile

--- src/main/java/com/funcoming/mapper/SedAwkMapper.java
+++ src/main/java/com/funcoming/mapper/SedAwkMapper.java
@@ -5,6 +5,8 @@ import org.apache.hadoop.io.NullWritable;
 import org.apache.hadoop.io.Text;
 import org.apache.hadoop.mapreduce.Mapper;
 
+import java.io.BufferedReader;
+import java.io.FileReader;
 import java.io.IOException;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
@@ -19,13 +21,26 @@ public class SedAwkMapper extends Mapper<LongWritable, Text, Text, NullWritable>
 
     @Override
     protected void map(LongWritable key, Text value, Context context) throws IOException, InterruptedException {
-        String[] split = value.toString().split("[|]");
-//        String[] split = value.toString().split("\\|");
-        Matcher matcher = this.patterner.matcher(split[split.length - 1]);
-        if (matcher.find()) {
-            String missingFields = context.getConfiguration().get("missingFields");
-            this.line.set(value.toString() + missingFields);
+//        String[] split = value.toString().split("[|]");
+////        String[] split = value.toString().split("\\|");
+//        Matcher matcher = this.patterner.matcher(split[split.length - 1]);
+//        if (matcher.find()) {
+//            String missingFields = context.getConfiguration().get("missingFields");
+//            this.line.set(value.toString() + missingFields);
+//            context.write(this.line, NullWritable.get());
+//        }
+/**
+ * 下面的代码是测试CacheFile。。。
+ * 把CacheFile的内容逐行写入到context中。。
+ *
+ */
+        BufferedReader bufferedReader = new BufferedReader(new FileReader("guoliufang.txt"));
+        String line = null;
+        while ((line = bufferedReader.readLine()) != null) {
+            this.line.set(line);
             context.write(this.line, NullWritable.get());
         }
+        bufferedReader.close();
+
     }
 }
